4922306ca3b7b0e5ca3dabf8f60dd9a062d6ba9d
rfaridi <rushad.16@gmail.com>
2015-12-30 00:13:56 +0600

    This is the commit I just set up the way I will continue web content preparation.

--- gr_capMach.R
+++ gr_capMach.R
@@ -1,5 +1,7 @@
 library(gtable)
 library(grid)
+library(dplyr)
+library(scales)
 source('functions.R')
 load('./RData/master.RData')
 load('./RData/Monthly_capMach_Exp.RData')
@@ -9,3 +11,9 @@ ggsave('./figs/cmach.png',plot_cmach,
 
 mo.dat <- mo.dat %>% 
   mutate(gr_cm=(cap_mach-lag(cap_mach))/lag(cap_mach))
+
+p <- ggplot(mo.dat,aes(x=dates,y=cap_mach))
+p1 <- p+geom_line()+scale_x_date(breaks=mo.dat$dates[c(seq(1,26,3),27)],labels=date_format('%b-%Y'))
+
+per <- ggplot(mo.dat,aes(x=dates,y=gr_cm))
+p2 <- per+geom_bar(stat='identity')+scale_x_date(breaks=mo.dat$dates[c(seq(1,26,3),27)],labels=date_format('%b-%Y')) 
