e1615ed0818f4e8c270e06fadf4960ef5367c349
rfaridi <rushad.16@gmail.com>
2016-01-03 16:04:31 +0600

    Fixed the plotting codes

--- gr_capMach.R
+++ gr_capMach.R
@@ -3,24 +3,42 @@ library(scales)
 source('functions.R')
 load('./RData/master.RData')
 load('./RData/Monthly_capMach_Exp.RData')
-plot_cmach <- lineCh(master,"cap_mach")
-ggsave('./figs/cmach.png',plot_cmach,
-       width=8.88,height=8.88*(1/1.618), dpi=72) 
 
 cm <- ggplot(mo.dat,aes(x=dates,
                         y=cap_mach))
-cm.plot <- p+geom_line()+
+cm.plot <- cm+geom_line()+ stat_smooth(fill=NA,
+                                       size=1,
+                                       color='red') + 
          scale_x_date(breaks=mo.dat$dates[c(seq(1,26,3),27)],
                       labels=date_format('%b-%Y')) + 
-        scale_y_continuous(limits = c(0,2100))
+        scale_y_continuous(limits = c(700,2100)) +
+                    themeLine + 
+        theme(axis.text.x=element_text(angle = 40,
+                                       vjust =.5))
+
+ggsave('./figs/cm_plot.png',
+       cm.plot,
+       width=8.88,
+       height=6,
+       dpi=72)
+
 
 cm.gw <- ggplot(mo.dat,aes(x=dates,
                            y=gr_cm))
-cm.gw.pl <- per+geom_bar(stat='identity')+
-                scale_x_date(breaks=mo.dat$dates[c(seq(1,26,3),27)],
-                labels=date_format('%b-%Y')) 
 
-ggsave('./figs/exports2.png',
+cm.gw.pl <- cm.gw+
+                  geom_line()+ 
+                         stat_smooth(fill=NA,
+                                     size=1,
+                                     color='red') + 
+                scale_x_date(breaks=mo.dat$dates[c(seq(1,26,3),27)],
+                labels=date_format('%b-%Y')) +
+             scale_y_continuous(labels = percent) +
+  themeLine + 
+  theme(axis.text.x=element_text(angle = 40,
+                                 vjust =.5))
+
+ggsave('./figs/cm_gw.png',
        cw.gw.pl,
        width=8.88,
        height=6,
