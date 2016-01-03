664d166cc5764121bcc2d4f7867ad6f5e321532e
rfaridi <rushad.16@gmail.com>
2016-01-03 15:21:03 +0600

    A little cleaner version

--- gr_capMach.R
+++ gr_capMach.R
@@ -1,5 +1,3 @@
-library(gtable)
-library(grid)
 library(dplyr)
 library(scales)
 source('functions.R')
@@ -7,13 +5,27 @@ load('./RData/master.RData')
 load('./RData/Monthly_capMach_Exp.RData')
 plot_cmach <- lineCh(master,"cap_mach")
 ggsave('./figs/cmach.png',plot_cmach,
-       width=8.88,height=6,dpi=72) 
+       width=8.88,height=8.88*(1/1.618), dpi=72) 
 
-mo.dat <- mo.dat %>% 
-  mutate(gr_cm=(cap_mach-lag(cap_mach))/lag(cap_mach))
+cm <- ggplot(mo.dat,aes(x=dates,
+                        y=cap_mach))
+cm.plot <- p+geom_line()+
+         scale_x_date(breaks=mo.dat$dates[c(seq(1,26,3),27)],
+                      labels=date_format('%b-%Y')) + 
+        scale_y_continuous(limits = c(0,2100))
 
-p <- ggplot(mo.dat,aes(x=dates,y=cap_mach))
-p1 <- p+geom_line()+scale_x_date(breaks=mo.dat$dates[c(seq(1,26,3),27)],labels=date_format('%b-%Y'))
+cm.gw <- ggplot(mo.dat,aes(x=dates,
+                           y=gr_cm))
+cm.gw.pl <- per+geom_bar(stat='identity')+
+                scale_x_date(breaks=mo.dat$dates[c(seq(1,26,3),27)],
+                labels=date_format('%b-%Y')) 
 
-per <- ggplot(mo.dat,aes(x=dates,y=gr_cm))
-p2 <- per+geom_bar(stat='identity')+scale_x_date(breaks=mo.dat$dates[c(seq(1,26,3),27)],labels=date_format('%b-%Y')) 
+ggsave('./figs/exports2.png',
+       cw.gw.pl,
+       width=8.88,
+       height=6,
+       dpi=72)
+
+
+
+
